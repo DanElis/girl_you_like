3e552b3e1b1fddb134b0def6c39cc4637c380851
Daniil Eliseev <deliseev@digital-roads.ru>
2023-01-15 16:20:17 +0100

    add method like_on_directory

--- backend/router.py
+++ backend/router.py
@@ -1,4 +1,6 @@
 import io
+import os
+from pathlib import Path
 
 import numpy as np
 from PIL import Image
@@ -13,9 +15,22 @@ router = APIRouter(prefix="/effects")
 
 
 @router.post("/like")
-async def apply_effect(file: UploadFile = File(...), type_predict: str = Form("all")):
+async def like(file: UploadFile = File(...), type_predict: str = Form("all")):
     if type_predict not in ['all', 'clear']:
         raise ValueError(f'type_predict must be all or clear. Get {type_predict}')
     img = Image.open(io.BytesIO(file.file.read()))
     img = np.array(img)
     return predict_like(img, type_predict)
+
+
+@router.post("/like_on_directory")
+async def like_on_directory(dir_path: str = Form(...), type_predict: str = Form("all")):
+    if type_predict not in ['all', 'clear']:
+        raise ValueError(f'type_predict must be all or clear. Get {type_predict}')
+    results = []
+    dir_path = Path(dir_path)
+    for img_name in os.listdir(dir_path):
+        img = Image.open(dir_path / img_name)
+        img = np.array(img)
+        results.append((img_name, predict_like(img, type_predict)))
+    return results
